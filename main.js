58598131889ee2a74de890a9a3ef3bbe943f352b
Naiceiry <developernay@gmail.com>
2022-10-11 14:33:13 +0200

    java8
    anexo certiicado de java 8

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ let cerrar=document.querySelectorAll(".close")[0];
 let cerrarG=document.querySelectorAll(".closeG")[0];
 let cerrarA=document.querySelectorAll(".closeA")[0];
 let cerrarjs=document.querySelectorAll(".closejs")[0];
+let cerrarj8=document.querySelectorAll(".closej8")[0];
 
 let abrir=document.querySelectorAll(".cta")[0];
 let abrirg=document.querySelectorAll(".ctag")[0];
 let abrira=document.querySelectorAll(".ctaa")[0];
 let abrirjs=document.querySelectorAll(".ctajs")[0];
+let abrirj8=document.querySelectorAll(".ctaj8")[0];
 
 
 let modal=document.querySelectorAll(".modal")[0];
@@ -14,6 +16,7 @@ let modalC=document.querySelectorAll(".modal-container")[0];
 let modalG=document.querySelectorAll(".modal-containerG")[0];
 let modalA=document.querySelectorAll(".modal-containerA")[0];
 let modaljs=document.querySelectorAll(".modal-containerjs")[0];
+let modalj8=document.querySelectorAll(".modal-containerj8")[0];
 
 abrir.addEventListener("click", function(e){
     e.preventDefault();
@@ -39,7 +42,12 @@ abrirjs.addEventListener("click", function(e){
     modaljs.style.visibility="visible";
     modal.classList.toggle("modal-close");
 });
-
+abrirj8.addEventListener("click", function(e){
+    e.preventDefault();
+    modalj8.style.opacity="1";
+    modalj8.style.visibility="visible";
+    modal.classList.toggle("modal-close");
+});
 
 cerrar.addEventListener("click", function(){
         modalC.style.opacity="0";
@@ -57,7 +65,10 @@ cerrarjs.addEventListener("click", function(){
     modaljs.style.opacity="0";
     modaljs.style.visibility="hidden";
 })
-
+cerrarj8.addEventListener("click", function(){
+    modalj8.style.opacity="0";
+    modalj8.style.visibility="hidden";
+})
 
 window.addEventListener("click", function(e){
     this.console.log(e.target )
@@ -96,6 +107,16 @@ window.addEventListener("click", function(e){
         setTimeout(function() {
             modaljs.style.opacity="0";
             modaljs.style.visibility="hidden";
+        }, 100)
+    }
+   
+})
+window.addEventListener("click", function(e){
+    this.console.log(e.target )
+    if (e.target == modalj8 )  {
+        setTimeout(function() {
+            modalj8.style.opacity="0";
+            modalj8.style.visibility="hidden";
         }, 100)
     }
    
